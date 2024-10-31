ccf4f2c28cd85f20818aba6b539536ea347b16ef
Adeola Yusuf <sururatadeolayusuf@gmail.com>
2024-10-31 08:52:20 -0500

    updates- adeola

--- Services/Search.cs
+++ Services/Search.cs
@@ -13,6 +13,19 @@ public class SearchService<T> where T : IItem
     {
         this.items = items;
     }
+    // searches list for items that match 
+    public List<T> SearchBy(Func<T, bool> predicate)
+    {
+        List<T> results = new List<T>();
+        foreach (var item in items)
+        {
+            // ff the item matches add 
+            if (predicate(item))
+            {
+                results.Add(item);
+            }
+        }
+        return results;
+    }
 
-   
 }
